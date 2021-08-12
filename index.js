61f37e145d50d45024f848e389c0d36ecf0db421
Bui Quang An <quangan186@gmail.com>
2021-08-13 00:27:02 +0700

    12/8/2021 (add image + read more button)

--- index.js
+++ index.js
@@ -6,3 +6,15 @@ scroll_up.addEventListener("click", function(){
     behavior: "smooth"
   });
 });
+
+const readMoreBtn = document.querySelector('.read-more-btn');
+const text = document.querySelector('.content');
+
+readMoreBtn.addEventListener("click", (e)=>{
+  text.classList.toggle('show-more');
+  if (readMoreBtn.innerText === 'Read More') {
+    readMoreBtn.innerText = 'Read Less';
+  } else {
+    readMoreBtn.innerText = 'Read More';
+  }
+})
